c24110393e0ff14dd84a6cd802db741314b15c8c
Cameron King <cameron.s.king@gmail.com>
2021-01-23 19:45:53 -0800

    add overlay for images and video

--- scripts.js
+++ scripts.js
@@ -20,3 +20,68 @@ function goToTop() {
     document.body.scrollTop = 0;
     document.documentElement.scrollTop = 0;
 }
+
+// image overlay handling
+var modal = document.querySelector(".modal");
+var modalImage = document.querySelector(".modal_image");
+Array.from(document.querySelectorAll(".img_thumbnail")).forEach(item => {
+    item.addEventListener("click", event => {
+        modal.style.display = "block";
+        modalImage.src = event.target.src;
+    })
+});
+window.onclick = function(event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+};
+
+// video overlay handling
+var videoModal0 = document.querySelector(".videoModal0");
+document.querySelector(".videoThumbnail0").addEventListener("click", event => {
+    videoModal0.style.display = "block";
+});
+
+var videoModal1 = document.querySelector(".videoModal1");
+document.querySelector(".videoThumbnail1").addEventListener("click", event => {
+    videoModal1.style.display = "block";
+});
+
+var videoModal2 = document.querySelector(".videoModal2");
+document.querySelector(".videoThumbnail2").addEventListener("click", event => {
+    videoModal2.style.display = "block";
+});
+
+var videoModal3 = document.querySelector(".videoModal3");
+document.querySelector(".videoThumbnail3").addEventListener("click", event => {
+    videoModal3.style.display = "block";
+});
+
+var videoModal4 = document.querySelector(".videoModal4");
+document.querySelector(".videoThumbnail4").addEventListener("click", event => {
+    videoModal4.style.display = "block";
+});
+
+window.onclick = function(event) {
+    if (event.target == videoModal0) {
+        videoModal0.style.display = "none";
+    }
+    if (event.target == videoModal1) {
+        videoModal1.style.display = "none";
+    }
+    if (event.target == videoModal2) {
+        videoModal2.style.display = "none";
+    }
+    if (event.target == videoModal3) {
+        videoModal3.style.display = "none";
+    }
+    if (event.target == videoModal4) {
+        videoModal4.style.display = "none";
+    }
+};
+
+// Array.from(document.querySelectorAll(".video_thumbnail")).forEach(item => {
+//     item.addEventListener("click", event => {
+//         videoModal0.style.display = "block";
+//     })
+// });
